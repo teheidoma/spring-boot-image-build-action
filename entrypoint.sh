ba3bbe338deb1746960868cab92896e90d00609e
teheidoma <novakovskii2002@gmail.com>
2024-04-10 18:27:01 +0300

    test

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,11 +2,12 @@
 
 SDKMAN_DIR=/root/.sdkman
 source /root/.sdkman/bin/sdkman-init.sh
-echo $1
-sdk install java $1
+sdk install java $INPUT_JDK-DIST
 chmod +x ./gradlew
 ./gradlew bootBuildImage > log
+cat log
 IMAGE_NAME=$( awk -F"'" '/^Successfully built image/ {split($2, parts, ":"); print parts[1]}' log)
 IMAGE_TAG=$( awk -F"'" '/^Successfully built image/ {split($2, parts, ":"); print parts[2]}' log)
-
+echo "IMAGE_NAME=$IMAGE_NAME" >> $GITHUB_OUTPUT
+echo "IMAGE_TAG=$IMAGE_TAG" >> $GITHUB_OUTPUT
 docker push $IMAGE_NAME:$IMAGE_TAG
