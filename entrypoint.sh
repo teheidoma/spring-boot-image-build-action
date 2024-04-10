a629f6191e247775a58583d614bb1b502d19fec0
teheidoma <novakovskii2002@gmail.com>
2024-04-10 17:54:42 +0300

    test

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,9 @@
-#!/bin/sh -l
+#!/bin/bash
 
-bash -c "source /root/.sdkman/bin/sdkman-init.sh \
-&& sdk install java $1 \
-&& sdk install gradle $2"
-cd /work
+source /root/.sdkman/bin/sdkman-init.sh
+sdk install java $1
+sdk install gradle $2
+
 ./gradlew bootBuildImage
 IMAGE_NAME=$( awk -F"'" '/^Successfully built image/ {split($2, parts, ":"); print parts[1]}' log)
 IMAGE_TAG=$( awk -F"'" '/^Successfully built image/ {split($2, parts, ":"); print parts[2]}' log)
